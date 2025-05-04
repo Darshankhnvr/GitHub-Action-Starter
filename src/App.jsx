393a5271471c3434198316123661dc6a3d550ca1
Darshan <darshankgoudahnvr@gmail.com>
2025-05-05 00:06:44 +0530

    new branch

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>Github Actions</h1>
       <h2>Complete course with 2 projects</h2>
       <h3>Test the sonar scan</h3>
+      <h4>this is using to check the sonar is working or not</h4>
     </>
   )
 }
